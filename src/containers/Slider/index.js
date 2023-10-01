7722a2129edb755bd4265d3f4de88fe64a5c38e5
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-10-01 13:41:08 +0200

    Add some comment and correct the index error

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -6,29 +6,44 @@ import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
+
+  // The current index of the slideshow
   const [index, setIndex] = useState(0);
+
+  // Sorted list of data.focus by month
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+
+  // This function call the next time after a timeout
   const nextCard = () => {
+    console.log(`index == ${index} compared to length == ${byDateDesc.length}`);
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
   };
+  
+  // This useEffect is called each time 'index' change,
+  // so each time a slide change it calls the next slide change
   useEffect(() => {
     nextCard();
   });
+
   return (
+    // Slideshow
     <div className="SlideCardList">
+      {/* For each slide */}
       {byDateDesc?.map((event, idx) => (
         <>
+          {/* Slide */}
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
-          >
+            >
+            
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -38,6 +53,8 @@ const Slider = () => {
               </div>
             </div>
           </div>
+
+          {/* Pagination */}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
